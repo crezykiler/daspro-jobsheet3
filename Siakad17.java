b757fbae2554d5fd55f085054eefbc3b898f87dd
zaki setyawan <kiminsetyawan@gmail.com>
2024-09-13 15:11:08 +0700

    2.1.1

--- Siakad17.java
+++ Siakad17.java
@@ -8,6 +8,28 @@ public class Siakad17 {
         String nama, nim;
         char kelas;
         byte absen;
-        double nilaiKuas
+        double nilaiKuis, nilaiTugas, nilaiUjian, nilaiAkhir;
+
+        System.out.print("Masukkan nama: ");
+        nama = mulyono.nextLine();
+        System.out.print("Masukkan nim: ");
+        nim = mulyono.nextLine();
+        System.out.print("Masukkan kelas: ");
+        kelas = mulyono.nextLine();
+        System.out.print("Masukkan absen: ");
+        absen = mulyonoc.nextByte();
+
+        System.out.print("Masukkan nilai kuis: ");
+        nilaiKuis = mulyono.nextLine();
+        System.out.print("Masukkan nilai tugas: ");
+        nilaiTugas = mulyono.nextLine();
+        System.out.print("Masukkan nilai ujian: ");
+        nilaiUjian = mulyono.nextLine();
+
+        nilaiAkhir = (nilaiKuis + nilaiTugas + nilaiUjian) / 3;
+
+        System.out.println("Nama: " + nama + "NIM: " + nim);
+        System.out.println("Kelas: " + kelas + "Absen: " + absen);
+        System.out.println("Nilai Akhir: " + nilaiAkhir);
     }
 }
