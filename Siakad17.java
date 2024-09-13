2d7930c1994d674a736431a32a7ada34da652be7
zaki setyawan <kiminsetyawan@gmail.com>
2024-09-13 15:00:42 +0700

    commit sayaa

---
+++ Siakad17.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Siakad17 {
+
+    public static void main(String[] args) {
+        Scanner Mulyono = new Scanner(System.in);
+
+        String nama, nim;
+        char kelas;
+        byte absen;
+        double nilaiKuas
+    }
+}
