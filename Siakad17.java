48225cb5f17a9f7ccd08f2e991002eeea47bdfd5
zaki setyawan <kiminsetyawan@gmail.com>
2024-09-14 06:16:32 +0700

    Comit Tugas NO.7

--- Siakad17.java
+++ Siakad17.java
@@ -5,28 +5,29 @@ public class Siakad17 {
     public static void main(String[] args) {
         Scanner Mulyono = new Scanner(System.in);
 
-        String nama, nim;
-        char kelas;
+        String nama, nim, kelas;
         byte absen;
-        double nilaiKuis, nilaiTugas, nilaiUjian, nilaiAkhir;
+        double nilaiKuis, nilaiTugas, nilaiUts, nilaiUas, nilaiAkhir;
 
         System.out.print("Masukkan nama: ");
         nama = Mulyono.nextLine();
-        System.out.print("Masukkan nim: ");
+        System.out.println("Masukkan nim: ");
         nim = Mulyono.nextLine();
-        System.out.print("Masukkan kelas: ");
-        kelas = Mulyono.nextLine().charAt(0);
+        System.out.println("Masukkan kelas: ");
+        kelas = Mulyono.nextLine();
         System.out.print("Masukkan absen: ");
         absen = Mulyono.nextByte();
 
-        System.out.print("Masukkan nilai kuis: ");
+        System.out.print("Masukkan nilai kuis: " );
         nilaiKuis = Mulyono.nextDouble();
         System.out.print("Masukkan nilai tugas: ");
         nilaiTugas = Mulyono.nextDouble();
-        System.out.print("Masukkan nilai ujian: ");
-        nilaiUjian = Mulyono.nextDouble();
+        System.out.print("Masukkan nilai uts: ");
+        nilaiUts = Mulyono.nextDouble();
+        System.out.print("Masukkan nilai uas: ");
+        nilaiUas = Mulyono.nextDouble();
 
-        nilaiAkhir = (nilaiKuis + nilaiTugas + nilaiUjian) / 3;
+        nilaiAkhir = (nilaiKuis * 0.20) + (nilaiTugas * 0.15) + (nilaiUts * 0.30) + (nilaiUas * 0.35);
 
         System.out.println("Nama: " + nama + "NIM: " + nim);
         System.out.println("Kelas: " + kelas + "Absen: " + absen);
